7dd3dd919d1244ee5cdc4fa98e6142d6ed743fd8
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-04-08 23:27:07 +0530

    Refactor AddEmployee class to extend JFrame and implement UI for adding employee details

--- src/employee/management/system/AddEmployee.java
+++ src/employee/management/system/AddEmployee.java
@@ -1,4 +1,25 @@
 package employee.management.system;
 
-public class AddEmployee {
+import javax.swing.*;
+import java.awt.*; // Don't forget this for Color and Font
+
+public class AddEmployee extends JFrame {
+
+    AddEmployee() {
+        getContentPane().setBackground(new Color(163, 255, 188));
+        setLayout(null); // Important for absolute positioning
+
+        JLabel heading = new JLabel("Add Employee Detail");
+        heading.setBounds(320, 30, 300, 50);
+        heading.setFont(new Font("Serif", Font.BOLD, 25));
+        add(heading);
+
+        setSize(900, 700);
+        setLocation(300, 50);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new AddEmployee(); // Everything is handled inside constructor now
+    }
 }
