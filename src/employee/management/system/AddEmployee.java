3958f19343a91b9ab3417d88a57153c4847edb36
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-04-09 10:43:13 +0530

    Enhance AddEmployee UI by adding fields for father's name, date of birth, salary, address, phone, email, education, designation, aadhar number, and employee ID

--- src/employee/management/system/AddEmployee.java
+++ src/employee/management/system/AddEmployee.java
@@ -1,14 +1,19 @@
 package employee.management.system;
 
 import javax.swing.*;
-import java.awt.*; // Don't forget this for Color and Font
+import java.awt.*;
+import com.toedter.calendar.JDateChooser;
+import java.util.Random;
 
 public class AddEmployee extends JFrame {
-    JTextField tname, tfname, taddress, tphone, taadhar, temail , tsalary , tdesignation;
+    JTextField tname, tfname, taddress, tphone, taadhar, temail, tsalary, tdesignation;
+    JDateChooser tdob;
+    JComboBox<String> Boxeducation;
+    JLabel tempid;
 
     AddEmployee() {
         getContentPane().setBackground(new Color(163, 255, 188));
-        setLayout(null); // Important for absolute positioning
+        setLayout(null);
 
         JLabel heading = new JLabel("Add Employee Detail");
         heading.setBounds(320, 30, 300, 50);
@@ -25,12 +30,117 @@ public class AddEmployee extends JFrame {
         tname.setBackground(new Color(177, 252, 197));
         add(tname);
 
+        JLabel fname = new JLabel("Father's Name");
+        fname.setBounds(400, 150, 150, 30);
+        fname.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
+        add(fname);
+
+        tfname = new JTextField();
+        tfname.setBounds(600, 150, 150, 30);
+        tfname.setBackground(new Color(177, 252, 197));
+        add(tfname);
+
+        JLabel dob = new JLabel("Date Of Birth");
+        dob.setBounds(50, 200, 150, 30);
+        dob.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
+        add(dob);
+
+        tdob = new JDateChooser();
+        tdob.setBounds(200, 200, 150, 30);
+        tdob.setBackground(new Color(177, 252, 197));
+        add(tdob);
+
+        JLabel salary = new JLabel("Salary");
+        salary.setBounds(400, 200, 150, 30);
+        salary.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
+        add(salary);
+
+        tsalary = new JTextField();
+        tsalary.setBounds(600, 200, 150, 30);
+        tsalary.setBackground(new Color(177, 252, 197));
+        add(tsalary);
+
+        JLabel address = new JLabel("Address");
+        address.setBounds(50, 250, 150, 30);
+        address.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
+        add(address);
+
+        taddress = new JTextField();
+        taddress.setBounds(200, 250, 150, 30);
+        taddress.setBackground(new Color(177, 252, 197));
+        add(taddress);
+
+        JLabel phone = new JLabel("Phone");
+        phone.setBounds(400, 250, 150, 30);
+        phone.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
+        add(phone);
+
+        tphone = new JTextField();
+        tphone.setBounds(600, 250, 150, 30);
+        tphone.setBackground(new Color(177, 252, 197));
+        add(tphone);
+
+        JLabel email = new JLabel("Email");
+        email.setBounds(50, 300, 150, 30);
+        email.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
+        add(email);
+
+        temail = new JTextField();
+        temail.setBounds(200, 300, 150, 30);
+        temail.setBackground(new Color(177, 252, 197));
+        add(temail);
+
+        JLabel education = new JLabel("Highest Education");
+        education.setBounds(400, 300, 200, 30);
+        education.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
+        add(education);
+
+        String[] items = {"BBA", "B.Tech", "BCA", "BA", "BSC", "B.COM", "MBA", "MCA", "MA", "MTech", "MSC", "PHD"};
+        Boxeducation = new JComboBox<>(items);
+        Boxeducation.setBounds(600, 300, 150, 30);
+        Boxeducation.setBackground(new Color(177, 252, 197));
+        add(Boxeducation);
+
+        JLabel designation = new JLabel("Designation");
+        designation.setBounds(50, 350, 150, 30);
+        designation.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
+        add(designation);
+
+        tdesignation = new JTextField();
+        tdesignation.setBounds(200, 350, 150, 30);
+        tdesignation.setBackground(new Color(177, 252, 197));
+        add(tdesignation);
+
+        JLabel aadhar = new JLabel("Aadhar Number");
+        aadhar.setBounds(400, 350, 150, 30);
+        aadhar.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
+        add(aadhar);
+
+        taadhar = new JTextField();
+        taadhar.setBounds(600, 350, 150, 30);
+        taadhar.setBackground(new Color(177, 252, 197));
+        add(taadhar);
+
+        JLabel empid = new JLabel("Employee ID");
+        empid.setBounds(50, 400, 150, 30);
+        empid.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
+        add(empid);
+
+        Random random = new Random();
+        int number = random.nextInt(999999);
+
+        tempid = new JLabel("" + number);
+        tempid.setBounds(200, 400, 150, 30);
+        tempid.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
+        tempid.setForeground(Color.RED);
+        add(tempid);
+
         setSize(900, 700);
         setLocation(300, 50);
         setVisible(true);
     }
 
     public static void main(String[] args) {
-        new AddEmployee(); // Everything is handled inside constructor now
+        new AddEmployee();
     }
 }
