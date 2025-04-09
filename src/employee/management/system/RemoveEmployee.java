a3e46d2f3036cecec7259d28827e4e0f350cab1c
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-04-09 23:00:05 +0530

    Refactor RemoveEmployee and UpdateEmployee classes for improved readability and maintainability by adding descriptive comments and formatting adjustments

--- src/employee/management/system/RemoveEmployee.java
+++ src/employee/management/system/RemoveEmployee.java
@@ -9,140 +9,151 @@ import java.awt.event.ItemListener;
 import java.sql.ResultSet;
 
 public class RemoveEmployee extends JFrame implements ActionListener {
-    Choice choiceEMPID;
-    JButton delete, back;
-    RemoveEmployee(){
+    Choice choiceEMPID; // Dropdown to select employee ID
+    JButton delete, back; // Buttons for delete and back actions
 
+    RemoveEmployee() {
+        // Label for Employee ID
         JLabel label = new JLabel("Employee ID");
-        label.setBounds(50,50,100,30);
-        label.setFont(new Font("Tahoma", Font.BOLD,15));
+        label.setBounds(50, 50, 100, 30);
+        label.setFont(new Font("Tahoma", Font.BOLD, 15));
         add(label);
 
+        // Dropdown to display employee IDs
         choiceEMPID = new Choice();
-        choiceEMPID.setBounds(200,50,150,30);
+        choiceEMPID.setBounds(200, 50, 150, 30);
         add(choiceEMPID);
 
-        try{
+        // Fetch employee IDs from the database and populate the dropdown
+        try {
             conn c = new conn();
             ResultSet resultSet = c.statement.executeQuery("select * from employee");
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 choiceEMPID.add(resultSet.getString("empId"));
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
+        // Labels and placeholders for employee details
         JLabel labelName = new JLabel("Name");
-        labelName.setBounds(50,100,100,30);
-        labelName.setFont(new Font("Tahoma", Font.BOLD,15));
+        labelName.setBounds(50, 100, 100, 30);
+        labelName.setFont(new Font("Tahoma", Font.BOLD, 15));
         add(labelName);
 
-        JLabel textName = new JLabel();
-        textName.setBounds(200,100,100,30);
+        JLabel textName = new JLabel(); // Placeholder for employee name
+        textName.setBounds(200, 100, 100, 30);
         add(textName);
 
         JLabel labelPhone = new JLabel("Phone");
-        labelPhone.setBounds(50,150,100,30);
-        labelPhone.setFont(new Font("Tahoma", Font.BOLD,15));
+        labelPhone.setBounds(50, 150, 100, 30);
+        labelPhone.setFont(new Font("Tahoma", Font.BOLD, 15));
         add(labelPhone);
 
-        JLabel textPhone = new JLabel();
-        textPhone.setBounds(200,150,100,30);
+        JLabel textPhone = new JLabel(); // Placeholder for employee phone
+        textPhone.setBounds(200, 150, 100, 30);
         add(textPhone);
 
         JLabel labelemail = new JLabel("Email");
-        labelemail.setBounds(50,200,100,30);
-        labelemail.setFont(new Font("Tahoma", Font.BOLD,15));
+        labelemail.setBounds(50, 200, 100, 30);
+        labelemail.setFont(new Font("Tahoma", Font.BOLD, 15));
         add(labelemail);
 
-        JLabel textEmail = new JLabel();
-        textEmail.setBounds(200,200,100,30);
+        JLabel textEmail = new JLabel(); // Placeholder for employee email
+        textEmail.setBounds(200, 200, 100, 30);
         add(textEmail);
 
+        // Fetch and display details of the selected employee
         try {
             conn c = new conn();
-            ResultSet resultSet = c.statement.executeQuery("select * from employee where empId = '"+choiceEMPID.getSelectedItem()+"'");
-            while (resultSet.next()){
+            ResultSet resultSet = c.statement.executeQuery("select * from employee where empId = '" + choiceEMPID.getSelectedItem() + "'");
+            while (resultSet.next()) {
                 textName.setText(resultSet.getString("name"));
                 textPhone.setText(resultSet.getString("phone"));
                 textEmail.setText(resultSet.getString("email"));
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
+        // Update employee details when a new ID is selected
         choiceEMPID.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
-                try{
+                try {
                     conn c = new conn();
-                    ResultSet resultSet = c.statement.executeQuery("select * from employee where empId = '"+choiceEMPID.getSelectedItem()+"'");
+                    ResultSet resultSet = c.statement.executeQuery("select * from employee where empId = '" + choiceEMPID.getSelectedItem() + "'");
                     while (resultSet.next()) {
                         textName.setText(resultSet.getString("name"));
                         textPhone.setText(resultSet.getString("phone"));
                         textEmail.setText(resultSet.getString("email"));
                     }
-                }catch (Exception E){
+                } catch (Exception E) {
                     E.printStackTrace();
                 }
             }
         });
 
+        // Delete button to remove the selected employee
         delete = new JButton("Delete");
-        delete.setBounds(80,300,100,30);
+        delete.setBounds(80, 300, 100, 30);
         delete.setBackground(Color.black);
         delete.setForeground(Color.WHITE);
         delete.addActionListener(this);
         add(delete);
 
+        // Back button to return to the previous screen
         back = new JButton("Back");
-        back.setBounds(220,300,100,30);
+        back.setBounds(220, 300, 100, 30);
         back.setBackground(Color.black);
         back.setForeground(Color.WHITE);
         back.addActionListener(this);
         add(back);
 
+        // Add an image icon for delete
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/delete.png"));
-        Image i2 = i1.getImage().getScaledInstance(200,200,Image.SCALE_DEFAULT);
+        Image i2 = i1.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel img = new JLabel(i3);
-        img.setBounds(700,80,200,200);
+        img.setBounds(700, 80, 200, 200);
         add(img);
 
+        // Add a background image
         ImageIcon i11 = new ImageIcon(ClassLoader.getSystemResource("icons/rback.png"));
-        Image i22 = i11.getImage().getScaledInstance(1120,630,Image.SCALE_DEFAULT);
+        Image i22 = i11.getImage().getScaledInstance(1120, 630, Image.SCALE_DEFAULT);
         ImageIcon i33 = new ImageIcon(i22);
         JLabel image = new JLabel(i33);
-        image.setBounds(0,0,1120,630);
+        image.setBounds(0, 0, 1120, 630);
         add(image);
 
-        setSize(1000,400);
-        setLocation(300,150);
+        // Set frame properties
+        setSize(1000, 400);
+        setLocation(300, 150);
         setLayout(null);
         setVisible(true);
-
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource()==delete){
+        if (e.getSource() == delete) {
+            // Delete the selected employee from the database
             try {
                 conn c = new conn();
-                String query = "delete from employee where empId = '"+choiceEMPID.getSelectedItem()+"'";
+                String query = "delete from employee where empId = '" + choiceEMPID.getSelectedItem() + "'";
                 c.statement.executeUpdate(query);
-                JOptionPane.showMessageDialog(null,"Employee Deleted Sucessfully");
+                JOptionPane.showMessageDialog(null, "Employee Deleted Successfully");
                 setVisible(false);
-                new Main_class();
-
-            }catch (Exception E){
+                new Main_class(); // Navigate to the main class
+            } catch (Exception E) {
                 E.printStackTrace();
             }
-        }else {
+        } else {
+            // Close the current window
             setVisible(false);
         }
     }
 
     public static void main(String[] args) {
-        new RemoveEmployee();
+        new RemoveEmployee(); // Launch the RemoveEmployee frame
     }
 }
--- src/employee/management/system/UpdateEmployee.java
+++ src/employee/management/system/UpdateEmployee.java
@@ -10,138 +10,153 @@ import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 
 public class UpdateEmployee extends JFrame implements ActionListener {
-    JTextField teducation, tfname, taddress,tphone, taadhar, temail, tsalary,tdesignation;
+    // Declare UI components and variables
+    JTextField teducation, tfname, taddress, tphone, taadhar, temail, tsalary, tdesignation;
     JLabel tempid;
-    JButton add,back;
+    JButton add, back;
     String number;
-    UpdateEmployee(String number){
 
+    // Constructor to initialize the UpdateEmployee frame
+    UpdateEmployee(String number) {
         this.number = number;
-        getContentPane().setBackground(new Color(163,255,188));
 
+        // Set background color for the frame
+        getContentPane().setBackground(new Color(163, 255, 188));
+
+        // Add heading label
         JLabel heading = new JLabel("Add Employee Detail");
-        heading.setBounds(320,30,500,50);
-        heading.setFont(new Font("serif", Font.BOLD,25));
+        heading.setBounds(320, 30, 500, 50);
+        heading.setFont(new Font("serif", Font.BOLD, 25));
         add(heading);
 
+        // Add Name label and text field
         JLabel name = new JLabel("Name");
-        name.setBounds(50,150,150,30);
-        name.setFont(new Font("SAN_SERIF", Font.BOLD,20));
+        name.setBounds(50, 150, 150, 30);
+        name.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
         add(name);
 
         JLabel tname = new JLabel();
-        tname.setBounds(200,150,150,30);
-        tname.setBackground(new Color(177,252,197));
+        tname.setBounds(200, 150, 150, 30);
+        tname.setBackground(new Color(177, 252, 197));
         add(tname);
 
+        // Add Father's Name label and text field
         JLabel fname = new JLabel("Father's Name");
-        fname.setBounds(400,150,150,30);
-        fname.setFont(new Font("SAN_SERIF", Font.BOLD,20));
+        fname.setBounds(400, 150, 150, 30);
+        fname.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
         add(fname);
 
         tfname = new JTextField();
-        tfname.setBounds(600,150,150,30);
-        tfname.setBackground(new Color(177,252,197));
+        tfname.setBounds(600, 150, 150, 30);
+        tfname.setBackground(new Color(177, 252, 197));
         add(tfname);
 
+        // Add Date of Birth label
         JLabel dob = new JLabel("Date Of Birth");
-        dob.setBounds(50,200,150,30);
-        dob.setFont(new Font("SAN_SERIF", Font.BOLD,20));
+        dob.setBounds(50, 200, 150, 30);
+        dob.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
         add(dob);
 
         JLabel tdob = new JLabel();
-        tdob.setBounds(200,200,150,30);
+        tdob.setBounds(200, 200, 150, 30);
         tdob.setFont(new Font("Tahoma", Font.BOLD, 20));
         add(tdob);
 
+        // Add Salary label and text field
         JLabel salary = new JLabel("Salary");
-        salary.setBounds(400,200,150,30);
-        salary.setFont(new Font("SAN_SERIF", Font.BOLD,20));
+        salary.setBounds(400, 200, 150, 30);
+        salary.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
         add(salary);
 
         tsalary = new JTextField();
-        tsalary.setBounds(600,200,150,30);
-        tsalary.setBackground(new Color(177,252,197));
+        tsalary.setBounds(600, 200, 150, 30);
+        tsalary.setBackground(new Color(177, 252, 197));
         add(tsalary);
 
+        // Add Address label and text field
         JLabel address = new JLabel("Address");
-        address.setBounds(50,250,150,30);
-        address.setFont(new Font("SAN_SERIF", Font.BOLD,20));
+        address.setBounds(50, 250, 150, 30);
+        address.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
         add(address);
 
-        taddress= new JTextField();
-        taddress.setBounds(200,250,150,30);
-        taddress.setBackground(new Color(177,252,197));
+        taddress = new JTextField();
+        taddress.setBounds(200, 250, 150, 30);
+        taddress.setBackground(new Color(177, 252, 197));
         add(taddress);
 
-
+        // Add Phone label and text field
         JLabel phone = new JLabel("Phone");
-        phone.setBounds(400,250,150,30);
-        phone.setFont(new Font("SAN_SERIF", Font.BOLD,20));
+        phone.setBounds(400, 250, 150, 30);
+        phone.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
         add(phone);
 
-        tphone= new JTextField();
-        tphone.setBounds(600,250,150,30);
-        tphone.setBackground(new Color(177,252,197));
+        tphone = new JTextField();
+        tphone.setBounds(600, 250, 150, 30);
+        tphone.setBackground(new Color(177, 252, 197));
         add(tphone);
 
+        // Add Email label and text field
         JLabel email = new JLabel("Email");
-        email.setBounds(50,300,150,30);
-        email.setFont(new Font("SAN_SERIF", Font.BOLD,20));
+        email.setBounds(50, 300, 150, 30);
+        email.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
         add(email);
 
-        temail= new JTextField();
-        temail.setBounds(200,300,150,30);
-        temail.setBackground(new Color(177,252,197));
+        temail = new JTextField();
+        temail.setBounds(200, 300, 150, 30);
+        temail.setBackground(new Color(177, 252, 197));
         add(temail);
 
+        // Add Highest Education label and text field
         JLabel education = new JLabel("Higest Education");
-        education.setBounds(400,300,150,30);
-        education.setFont(new Font("SAN_SERIF", Font.BOLD,20));
+        education.setBounds(400, 300, 150, 30);
+        education.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
         add(education);
 
-        teducation= new JTextField();
-        teducation.setBounds(600,300,150,30);
-        teducation.setBackground(new Color(177,252,197));
+        teducation = new JTextField();
+        teducation.setBounds(600, 300, 150, 30);
+        teducation.setBackground(new Color(177, 252, 197));
         add(teducation);
 
+        // Add Aadhar Number label
         JLabel aadhar = new JLabel("Aadhar Number");
-        aadhar.setBounds(400,350,150,30);
-        aadhar.setFont(new Font("SAN_SERIF", Font.BOLD,20));
+        aadhar.setBounds(400, 350, 150, 30);
+        aadhar.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
         add(aadhar);
 
-        JLabel taadhar= new JLabel();
-        taadhar.setBounds(600,350,150,30);
-        taadhar.setBackground(new Color(177,252,197));
+        JLabel taadhar = new JLabel();
+        taadhar.setBounds(600, 350, 150, 30);
+        taadhar.setBackground(new Color(177, 252, 197));
         add(taadhar);
 
-
+        // Add Employee ID label
         JLabel empid = new JLabel("Employee ID");
-        empid.setBounds(50,400,150,30);
-        empid.setFont(new Font("SAN_SERIF", Font.BOLD,20));
+        empid.setBounds(50, 400, 150, 30);
+        empid.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
         add(empid);
 
-        tempid= new JLabel();
-        tempid.setBounds(200,400,150,30);
-        tempid.setFont(new Font("SAN_SARIF", Font.BOLD,20));
+        tempid = new JLabel();
+        tempid.setBounds(200, 400, 150, 30);
+        tempid.setFont(new Font("SAN_SARIF", Font.BOLD, 20));
         tempid.setForeground(Color.RED);
         add(tempid);
 
+        // Add Designation label and text field
         JLabel designation = new JLabel("Designation");
-        designation.setBounds(50,350,150,30);
-        designation.setFont(new Font("SAN_SERIF", Font.BOLD,20));
+        designation.setBounds(50, 350, 150, 30);
+        designation.setFont(new Font("SAN_SERIF", Font.BOLD, 20));
         add(designation);
 
-        tdesignation= new JTextField();
-        tdesignation.setBounds(200,350,150,30);
-        tdesignation.setBackground(new Color(177,252,197));
+        tdesignation = new JTextField();
+        tdesignation.setBounds(200, 350, 150, 30);
+        tdesignation.setBackground(new Color(177, 252, 197));
         add(tdesignation);
 
+        // Fetch employee details from the database
         try {
             conn c = new conn();
-            String query = "select * from employee where empId = '"+number+"'";
+            String query = "select * from employee where empId = '" + number + "'";
             ResultSet resultSet = c.statement.executeQuery(query);
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 tname.setText(resultSet.getString("name"));
                 tfname.setText(resultSet.getString("fname"));
                 tdob.setText(resultSet.getString("dob"));
@@ -150,64 +165,69 @@ public class UpdateEmployee extends JFrame implements ActionListener {
                 tphone.setText(resultSet.getString("phone"));
                 temail.setText(resultSet.getString("email"));
                 teducation.setText(resultSet.getString("education"));
-                taadhar.setText(resultSet.getString("addhar"));
+                taadhar.setText(resultSet.getString("aadhar"));
                 tempid.setText(resultSet.getString("empId"));
                 tdesignation.setText(resultSet.getString("designation"));
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-
+        // Add Update button
         add = new JButton("UPDATE");
-        add.setBounds(450,550,150,40);
+        add.setBounds(450, 550, 150, 40);
         add.setBackground(Color.black);
         add.setForeground(Color.WHITE);
         add.addActionListener(this);
         add(add);
 
+        // Add Back button
         back = new JButton("BACK");
-        back.setBounds(250,550,150,40);
+        back.setBounds(250, 550, 150, 40);
         back.setBackground(Color.black);
         back.setForeground(Color.WHITE);
         back.addActionListener(this);
         add(back);
 
-
-        setSize(900,700);
+        // Set frame properties
+        setSize(900, 700);
         setLayout(null);
-        setLocation(300,50);
+        setLocation(300, 50);
         setVisible(true);
-
     }
 
+    // Handle button click events
     @Override
     public void actionPerformed(ActionEvent e) {
-      if (e.getSource() == add){
-          String fname = tfname.getText();
-          String salary = tsalary.getText();
-          String address = taddress.getText();
-          String phone = tphone.getText();
-          String email = temail.getText();
-          String education = teducation.getText();
-          String designation = tdesignation.getText();
+        if (e.getSource() == add) {
+            // Fetch updated details from text fields
+            String fname = tfname.getText();
+            String salary = tsalary.getText();
+            String address = taddress.getText();
+            String phone = tphone.getText();
+            String email = temail.getText();
+            String education = teducation.getText();
+            String designation = tdesignation.getText();
 
-          try {
-              conn c = new conn();
-              String query = "update employee set fname = '"+fname+"', salary = '"+salary+"', address = '"+address+"', phone = '"+phone+"', email = '"+email+"', education = '"+education+"', designation = '"+designation+"' where empId = '"+number+"'";
-              c.statement.executeUpdate(query);
-              JOptionPane.showMessageDialog(null, "Details updated successfully");
-              setVisible(false);
-              new Main_class();
-          }catch (Exception E){
-              E.printStackTrace();
-          }
-      }else {
-          setVisible(false);
-          new View_Employee();
-      }
+            // Update employee details in the database
+            try {
+                conn c = new conn();
+                String query = "update employee set fname = '" + fname + "', salary = '" + salary + "', address = '" + address + "', phone = '" + phone + "', email = '" + email + "', education = '" + education + "', designation = '" + designation + "' where empId = '" + number + "'";
+                c.statement.executeUpdate(query);
+                JOptionPane.showMessageDialog(null, "Details updated successfully");
+                setVisible(false);
+                new Main_class();
+            } catch (Exception E) {
+                E.printStackTrace();
+            }
+        } else {
+            // Navigate back to the previous screen
+            setVisible(false);
+            new View_Employee();
+        }
     }
 
+    // Main method to run the application
     public static void main(String[] args) {
         new UpdateEmployee("");
     }
